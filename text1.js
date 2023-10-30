18b036c3573c06ecf5c71d0f25cd3363574f03ae
Jiahe(Paddy) ZHAO <86382862+PaddyZz@users.noreply.github.com>
2023-10-30 15:26:41 +0800

    Update text1.js

--- text1.js
+++ text1.js
@@ -1,5 +1,13 @@
-function playVideo() {
-    const video = document.getElementById('myVideo');
-    video.play();
-  }
+document.addEventListener("DOMContentLoaded", function(event) {
   
+  var waitingAnimations = document.querySelectorAll(".video-text span");
+
+  for (var i = 0; i < waitingAnimations.length; i++) {
+    (function(index) {
+      
+      setTimeout(function() {
+        waitingAnimations[index].style.animationPlayState = "running";
+      }, (index+1) * 600);
+    })(i);
+  }
+});
